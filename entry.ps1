c448394ede077f1f5d0425e169cf58f7358c8db3
Sebastian Gräf <segraef@microsoft.com>
2020-03-24 12:14:11 +0100

    removed some error handling

--- entry.ps1
+++ entry.ps1
@@ -11,18 +11,14 @@ if ($clientId -and $clientSecret -and $tenantId) {
   $credentials = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $clientId, $secureClientSecret
   Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $tenantId
 }
-else {
-  Write-Output "One of the required parameters for Azure Login is not set: clientId, clientSecret or tenantId"
-  exit
-}
 
 if ($identity -like "yes") {
   Connect-AzAccount -Identity
+  Get-AzContext
 }
 
 if ($subscriptionId) {
   Set-AzContext $subscriptionId
-  Get-AzContext
 }
 else {
   $subscriptions = Get-AzSubscription
