fc19b1c6a51cb07ba8b47b9ea9b30912edacce80
Sebastian Gräf <segraef@microsoft.com>
2020-03-29 13:35:47 +0200

    added environment support for Azure Gov

--- entry.ps1
+++ entry.ps1
@@ -3,27 +3,48 @@ Param(
   [string]$clientSecret,
   [string]$tenantId,
   [string]$subscriptionId,
-  [string]$identity
+  [string]$identity,
+  [string]$environmentName
 )
-
-if ($clientId -and $clientSecret -and $tenantId) {
-  $secureClientSecret = ConvertTo-SecureString -String "$clientSecret" -AsPlainText -Force
-  $credentials = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $clientId, $secureClientSecret
-  Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $tenantId
-}
-
-if ($identity -like "yes") {
-  Connect-AzAccount -Identity
-  Get-AzContext
-}
-
-if ($subscriptionId) {
-  Set-AzContext $subscriptionId
-}
-else {
-  $subscriptions = Get-AzSubscription
-  if (-not $subscriptions) {
-    Write-Output "subscriptionId is not set."
-    exit
+Try {
+  if ($clientId -and $clientSecret -and $tenantId) {
+    Write-Output "##########`nConnecting to Azure with service principal ...`n##########"
+    $secureClientSecret = ConvertTo-SecureString -String "$clientSecret" -AsPlainText -Force
+    $credentials = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $clientId, $secureClientSecret
+    Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $tenantId
+    Get-AzContext
   }
+
+  if ($clientId -and $clientSecret -and $tenantId -and $environmentName) {
+    Write-Output "##########`nAn Azure environment was given.`n"
+    Write-Output "Connecting to Azure with service principal ...`n##########"
+    $secureClientSecret = ConvertTo-SecureString -String "$clientSecret" -AsPlainText -Force
+    $credentials = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $clientId, $secureClientSecret
+    Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $tenantId -EnvironmentName $environmentName
+    Get-AzContext
+  }
+
+  if ($identity -like "yes") {
+    Write-Output "##########`nConnecting to Azure with identity ...`n##########"
+    Connect-AzAccount -Identity
+    Get-AzContext
+  }
+
+  if (($identity -like "yes") -and $environmentName) {
+    Write-Output "##########`nAzure environment $environmentName was given.`n"
+    Write-Output "Connecting to Azure with identity ...`n##########"
+    Connect-AzAccount -Identity -EnvironmentName $environmentName
+    Get-AzContext
+  }
+
+  if ($subscriptionId) {
+    Write-Output "##########`nAn Azure subscription was given."
+    Write-Output "Selecting subscription $subscriptionId ...`n##########"
+    Set-AzContext -Subscription $subscriptionId
+  }
+}
+Catch {
+  $_.Exception.Message
+  $_.Exception.ItemName
+  Throw
 }
